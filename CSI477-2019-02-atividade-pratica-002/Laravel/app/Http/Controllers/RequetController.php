9fcbbb5030e7ebc7feded0707a866b0c37a656ab
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2019-12-03 20:28:34 -0300

    03-12

---
+++ CSI477-2019-02-atividade-pratica-002/Laravel/app/Http/Controllers/RequetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Requet;
+use Illuminate\Http\Request;
+
+class RequetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Requet  $requet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Requet $requet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Requet  $requet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Requet $requet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Requet  $requet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Requet $requet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Requet  $requet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Requet $requet)
+    {
+        //
+    }
+}
---
+++ CSI477-2019-02-atividade-pratica-002/Laravel/database/factories/RequetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Requet;
+use Faker\Generator as Faker;
+
+$factory->define(Requet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
